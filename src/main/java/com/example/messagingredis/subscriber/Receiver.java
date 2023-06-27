89dec545421128342aaa6ad7b763570f4782017a
tokitaka <artigen3@gmail.com>
2023-06-28 00:50:17 +0900

    Register Listener and Send a Message

--- src/main/java/com/example/messagingredis/subscriber/Receiver.java
+++ src/main/java/com/example/messagingredis/subscriber/Receiver.java
@@ -4,16 +4,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.redis.connection.Message;
-import org.springframework.data.redis.connection.MessageListener;
 
-public class Receiver implements MessageListener{
-    
-    @Override
-    public void onMessage(Message message, byte[] bytes) {
-       LOGGER.info("Received <" +message + ">");
-        
-    }
+public class Receiver {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
 
