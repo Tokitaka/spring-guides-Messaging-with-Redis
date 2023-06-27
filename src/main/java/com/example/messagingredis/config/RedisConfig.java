fb7fed0e7656c7fd408168e7460e5ce6807f0084
tokitaka <artigen3@gmail.com>
2023-06-27 23:46:07 +0900

    The Receiver Class as a message listener

---
+++ src/main/java/com/example/messagingredis/config/RedisConfig.java
@@ -0,0 +1,45 @@
+package com.example.messagingredis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public JedisConnectionFactory connectionFactory(){
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName("localhost");
+        configuration.setPort(5555);
+        return new JedisConnectionFactory(configuration);
+
+    }
+    @Bean
+    public RedisTemplate<String, Object> template(){
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory());
+        template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+        return template;
+    }
+    @Bean
+    public ChannelTopic topic(){
+        return new ChannelTopic("messaging-redis");
+    }
+    @Bean
+    public MessageListenerAdapter messageListenerAdapter(){
+        return new MessageListenerAdapter();
+    }
+    
+    public RedisMessageListenerContainer redisMessageListenerContainer(){
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory());
+        container.addMessageListener(messageListenerAdapter(), topic());
+            return container;
+        }
+    }
--- src/main/java/com/example/messagingredis/subscriber/Receiver.java
+++ src/main/java/com/example/messagingredis/subscriber/Receiver.java
@@ -1,11 +1,20 @@
-package com.example.messagingredis;
+package com.example.messagingredis.subscriber;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
 
-public class Receiver {
+public class Receiver implements MessageListener{
+    
+    @Override
+    public void onMessage(Message message, byte[] bytes) {
+       LOGGER.info("Received <" +message + ">");
+        
+    }
+
     private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
 
     private AtomicInteger counter = new AtomicInteger();
